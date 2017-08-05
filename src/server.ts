0741ca132a3e6eec5b75ddabd297b703c57c94c3
Carlos Castillo Alarcón <castillo.st@gmail.com>
2017-08-05 02:02:35 +0200

    Add app bootstraping

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import * as bodyParser from 'body-parser';
+import * as express from 'express';
+import { Starter } from './component/server/starter';
+import { Configuration } from "./config/configuration";
 import { ApplicationModule } from './modules/app.module';
 
-const app = NestFactory.create(ApplicationModule);
-app.listen(3000, () => console.log('Application is listening on port 3000.'));
+const instance = express();
+instance.use(bodyParser.json());
+
+const app = NestFactory.create(ApplicationModule, instance);
+
+const starter = new Starter(new Configuration(), app);
+starter.start();
