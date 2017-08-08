7c485a0fc746e80dc759780fbd83362eb92e5664
Carlos Castillo Alarcón <castillo.st@gmail.com>
2017-08-09 00:44:28 +0200

    Add Youtube URL validator and peer

--- src/common/stream/youtube/youtube-url-validator.pipe.ts
+++ src/common/stream/youtube/youtube-url-validator.pipe.ts
@@ -1,8 +1,16 @@
-import { ArgumentMetadata, Pipe, PipeTransform } from '@nestjs/common';
+import { ArgumentMetadata, HttpStatus, Pipe, PipeTransform } from '@nestjs/common';
+import { HttpException } from '@nestjs/core';
+import { YoutubeUrlValidator } from './youtube-url-validator';
 
 @Pipe()
 export class YoutubeUrlValidatorPipe implements PipeTransform {
     public transform(value: any, metadata: ArgumentMetadata): any {
+        const validation = new YoutubeUrlValidator(value);
 
+        if (!validation.isValid) {
+            throw new HttpException(validation.message, HttpStatus.BAD_REQUEST);
+        }
+
+        return value;
     }
 }
